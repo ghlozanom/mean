09236c7e56d8e27f3258423475446e429a7123c9
Gabriel Lozano <ghlm81@hotmail.com>
2018-11-01 19:55:20 +0100

    End of the first rest API

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -6,28 +6,34 @@ employeeCtrl.getEmployees = async (req, res) => {
     res.json(employees);
 }
 
-employeeCtrl.createEmployee = (req, res) => {
-    res.json({
-        status: 'Employees come here'
-    });    
+employeeCtrl.createEmployee = async (req, res) => {
+    const employee = new Employee(req.body);
+    await employee.save();
+    res.json( {
+        'status' : 'Employee saved'
+    });
 }
 
-employeeCtrl.getEmployee = (req, res) => {
-    res.json({
-        status: 'Employees come here'
-    });    
+employeeCtrl.getEmployee = async (req, res) => {
+    const employee = await Employee.findById(req.params.id);
+    res.json(employee);
 }
 
-employeeCtrl.editEmployee = (req, res) => {
-    res.json({
-        status: 'Employees come here'
-    });    
+employeeCtrl.editEmployee = async (req, res) => {
+    const { id } = req.params;
+    const employee = {
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary
+    };
+    await Employee.findByIdAndUpdate(id, {$set: employee}, {new: true});
+    res.json( {'status' : 'Employee Updated'});
 }
 
-employeeCtrl.deleteEmployee = (req, res) => {
-    res.json({
-        status: 'Employees come here'
-    });   
+employeeCtrl.deleteEmployee = async (req, res) => {
+    await Employee.findByIdAndRemove(req.params.id);
+    res.json( {'status' : 'Employee Deleted'});
 }
 
 module.exports = employeeCtrl;
